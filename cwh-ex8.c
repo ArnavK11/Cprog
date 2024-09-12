78c44614112916f54968d9c6eac813c3d3b67df7
ArnavK11 <87559614+ArnavK11@users.noreply.github.com>
2024-09-12 16:47:52 +0530

    Update cwh-ex8.c

--- cwh-ex8.c
+++ cwh-ex8.c
@@ -3,72 +3,24 @@
 
 int main()
 {
-    int n,o,p;
+    int i, n = 1;
 
     char *ptr;
-
-    // Id of E1
-
-    printf("For Employee 1: \n");
-
-    printf("Enter the size of your Employee Id: ");
-    scanf("%d", &n);
-
-    ptr = (char *)malloc(n * sizeof(char));
-
-    printf("Enter your Employee Id\n");
-    for (int i = 0; i < n; i++)
+    while (n <= 3)
     {
-        scanf(" %c", &ptr[i]);
-    }
+        printf("For Employee %d:\nEnter the size of your Employee Id: ", n);
+        scanf("%d", &i);
 
-    printf("Employee Id of Employee 1 is: ");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%c", ptr[i]);
-    }
+        ptr = (char *)malloc(i * sizeof(char));
 
-    // Id of E2
+        printf("Enter your Employee Id\n");
+        scanf("%s", ptr);
 
-    printf("\nFor Employee 2: \n");
+        printf("Employee Id of Employee %d is: %s\n", n, ptr);
 
-    printf("Enter the size of your Employee Id: ");
-    scanf("%d", &o);
+        free(ptr);
 
-    ptr = (char *)realloc(ptr, o * sizeof(char));
-
-    printf("Enter your Employee Id\n");
-    for (int i = 0; i < o; i++)
-    {
-        scanf(" %c", &ptr[i]);
-    }
-
-    printf("Employee Id of Employee 2 is: ");
-    for (int i = 0; i < o; i++)
-    {
-        printf("%c", ptr[i]);
-    }
-
-    // Id of E3 
-
-
-    printf("\nFor Employee 3: \n");
-
-    printf("Enter the size of your Employee Id: ");
-    scanf("%d", &p);
-
-    ptr = (char *)realloc(ptr, p * sizeof(char));
-
-    printf("Enter your Employee Id\n");
-    for (int i = 0; i < p; i++)
-    {
-        scanf(" %c", &ptr[i]);
-    }
-
-    printf("Employee Id of Employee 3 is: ");
-    for (int i = 0; i < p; i++)
-    {
-        printf("%c", ptr[i]);
+        n++;
     }
     return 0;
 }
